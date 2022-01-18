9586b33afb9b90c512ef90c62adb779e8a38ce1f
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-18 19:33:39 -0300

    Simple Sum

---
+++ 1003.c
@@ -0,0 +1,20 @@
+/*
+    @autor: Guiventura6;
+    @data: 18/01/2022;
+    @Problema: Simple Sum;
+*/
+#include <stdio.h>
+
+int main(){
+    
+    int A, B, SOMA;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    SOMA = A + B;
+
+    printf("SOMA = %d\n", SOMA);
+        
+    return 0;
+}
