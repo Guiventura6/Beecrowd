62b9b9d8e3c15e907599fb2c65e94b8fb1c57224
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-18 19:14:16 -0300

    Extremely Basic

---
+++ 1001.c
@@ -0,0 +1,20 @@
+/*
+    @autor: Guiventura6;
+    @data: 18/01/2022;
+    @Problema: Extremely Basic;
+*/
+#include<stdio.h>
+
+int main(){
+    
+    int A, B, X;
+
+    scanf("%d", &A);
+    scanf("%d", &B);
+
+    X = A + B;
+
+    printf("X = %d\n", X);
+
+    return 0;
+}
