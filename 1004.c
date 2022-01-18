b6fed54ea6683bf489e85c6366efd9b4f202df00
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-18 19:42:09 -0300

    Simple Product

---
+++ 1004.c
@@ -0,0 +1,20 @@
+/*
+    @autor: Guiventura6;
+    @data: 18/01/2022;
+    @Problema: Simple Product;
+*/
+#include <stdio.h>
+
+int main(){
+    
+    int X, Y, PROD;
+
+    scanf("%d", &X);
+    scanf("%d", &Y);
+
+    PROD = X * Y;
+
+    printf("PROD = %d\n", PROD);
+
+    return 0;
+}
