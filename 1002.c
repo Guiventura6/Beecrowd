d3a2f751e5ff885b7a5a32a6a28eea24842c5d57
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-18 19:24:27 -0300

    Area of a Circle

---
+++ 1002.c
@@ -0,0 +1,18 @@
+/*
+    @autor: Guiventura6;
+    @data: 18/01/2022;
+    @Problema: Area of a Circle;
+*/
+
+int main(){
+    
+    double R, A, PI = 3.14159;
+
+    scanf("%lf", &R);
+
+    A = PI * R * R;
+
+    printf("A=%.4lf\n", A);
+    
+    return 0;
+}
