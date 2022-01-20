d280a44807a1867fc7e3a77fa25bf7c60d560022
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-20 16:28:07 -0300

    Distance

---
+++ 1016.c
@@ -0,0 +1,19 @@
+/*  
+    @autor: Guiventura6;
+    @data: 20/01/2022;
+    @Problema: Distance;
+*/
+#include <stdio.h>
+
+int main(){
+    
+    int distanceKm, qtdMinutes;
+
+    scanf("%d", &distanceKm);
+
+    qtdMinutes = distanceKm * 2;
+
+    printf("%d minutos\n", qtdMinutes);
+        
+    return 0;
+}
