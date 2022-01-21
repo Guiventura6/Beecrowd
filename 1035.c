133a8a95861072548fbabf1af600c8ef367368c2
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-21 12:20:16 -0300

    Selection Test 1

---
+++ 1035.c
@@ -0,0 +1,22 @@
+/*  
+    @autor: Guiventura6;
+    @data: 21/01/2022;
+    @Problema: Selection Test 1;    
+*/
+#include <stdio.h>
+
+int main(){
+
+    int a, b, c, d;
+
+    scanf("%d%d%d%d", &a, &b, &c, &d);
+
+    if (b > c && d > a && c + d > a + b && c >= 0 && d >= 0 && a % 2 == 0) {         
+        printf("Valores aceitos\n");                   
+    }
+    else {
+        printf("Valores nao aceitos\n");
+    }
+
+    return 0;
+}
