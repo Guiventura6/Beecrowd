acc6e3ba67e7a8a208ac5921ef5d4d165efb542e
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-21 14:07:35 -0300

    Bhaskara's Formula

---
+++ 1036.c
@@ -0,0 +1,27 @@
+/*  
+    @autor: Guiventura6;
+    @data: 21/01/2022;
+    @Problema: Bhaskara's Formula;    
+*/
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+    
+    double a, b, c;
+
+    scanf("%lf%lf%lf", &a, &b, &c);
+
+    double delta = pow(b, 2.0) - 4 * a * c;
+
+    if (a == 0 || delta < 0) {
+        printf("Impossivel calcular\n");
+    }else {        
+        double r1 = (-b + sqrt(delta)) / (2 * a);
+        double r2 = (-b - sqrt(delta)) / (2 * a);
+        printf("R1 = %.5lf\n", r1);
+        printf("R2 = %.5lf\n", r2);
+    }
+
+    return 0;
+}
