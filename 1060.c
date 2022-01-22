0d3a8ed3d228ebe216e69c410371ff27b7096eca
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-22 18:51:38 -0300

    Positive Numbers

---
+++ 1060.c
@@ -0,0 +1,23 @@
+/*  
+    @autor: Guiventura6;
+    @data: 22/01/2022;
+    @Problema: Positive Numbers;
+*/
+#include <string.h>
+
+int main() {
+
+    int i, cont = 0;
+    double n;
+
+    for (i = 0; i < 6; i++)
+    {
+        scanf("%lf", &n);
+        if(n > 0){
+            cont++;
+        }
+    }
+    printf("%d valores positivos\n", cont);
+            
+    return 0;
+}
