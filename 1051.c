4c871603aebc86bb7abc7a3f7f1c6da04a84e38e
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-22 18:12:01 -0300

    Taxes

---
+++ 1051.c
@@ -0,0 +1,28 @@
+/*  
+    @autor: Guiventura6;
+    @data: 22/01/2022;
+    @Problema: Taxes;
+*/
+#include <string.h>
+
+int main () {
+    
+    double salary, totalTax = 0.0;
+
+    scanf("%lf", &salary);
+
+    if (salary < 2000.00) {
+        printf("Isento\n");
+    } else {
+        if (salary <= 3000.00) {
+            totalTax = (salary - 2000.00) * 0.08;
+        } else if (salary <= 4500.00) {
+            totalTax = (salary - 3000.00) * 0.18 + 1000.00 * 0.08;
+        } else {
+            totalTax = (salary - 4500.00) * 0.28 + 1500.00 * 0.18 + 1000.00 * 0.08;
+        }
+        printf("R$ %.2lf\n", totalTax);
+    }
+
+    return 0;
+}
