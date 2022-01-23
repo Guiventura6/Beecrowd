654bf77317d58654053915aee8ead40a0583ee8d
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-22 21:52:36 -0300

    Odd Numbers

---
+++ 1067.c
@@ -0,0 +1,21 @@
+/*  
+    @autor: Guiventura6;
+    @data: 22/01/2022;
+    @Problema: Odd Numbers;
+*/
+#include <string.h>
+
+int main (){
+    
+    int i, number;
+    
+    scanf("%d", &number);
+
+    for (i = 1; i <= number; i++) {
+        if (i % 2 != 0) {
+            printf("%d\n", i);
+        }            
+    }
+            
+    return 0;
+}
