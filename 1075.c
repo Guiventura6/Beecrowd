10243659cd354e2307420e6f63f0673ca5b5558a
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-23 09:10:17 -0300

    Remaining 2

---
+++ 1075.c
@@ -0,0 +1,21 @@
+/*  
+    @autor: Guiventura6;
+    @data: 23/01/2022;
+    @Problema: Remaining 2;
+*/
+#include <stdio.h>
+
+int main() {
+    
+    int i, n;
+
+    scanf("%d", &n);
+
+    for (i = 1; i < 10000; i++) {
+        if (i % n == 2) {
+            printf("%d\n", i);
+        }        
+    }
+
+    return 0;
+}
