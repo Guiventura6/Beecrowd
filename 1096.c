1406b428cbda948cf87b14f29fd2199b24dd8846
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-23 11:37:59 -0300

    Sequence IJ 2

---
+++ 1096.c
@@ -0,0 +1,19 @@
+/*  
+    @autor: Guiventura6;
+    @data: 23/01/2022;
+    @Problema: Sequence IJ 2;
+*/
+#include <stdio.h>
+
+int main () {
+
+    int i, j;
+    
+    for (i = 1; i <= 9; i += 2) {
+        for (j = 7; j >= 5; j--) {
+            printf("I=%d J=%d\n", i, j);
+        }                
+    }
+        
+    return 0;
+}
