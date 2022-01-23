cd55b98e6aaf154051c2a7bc755951b810e28e77
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-22 21:15:28 -0300

    Even Between five Numbers

---
+++ 1065.c
@@ -0,0 +1,25 @@
+/*  
+    @autor: Guiventura6;
+    @data: 22/01/2022;
+    @Problema: Even Between five Numbers;
+*/
+#include <string.h>
+
+int main () {
+    
+    int i, number[10], cont = 0;
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &number[i]);
+    }
+
+    for (i = 0; i < 5; i++) {
+        if (number[i] % 2 == 0) {
+            cont++;
+        }        
+    }
+
+    printf("%d valores pares\n", cont);
+            
+    return 0;
+}
