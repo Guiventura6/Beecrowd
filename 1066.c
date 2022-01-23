1471039fa010a634a00d04e7231ff8c3849faced
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-22 21:46:29 -0300

    Even, Odd, Positive and Negative

---
+++ 1066.c
@@ -0,0 +1,36 @@
+/*  
+    @autor: Guiventura6;
+    @data: 22/01/2022;
+    @Problema: Even, Odd, Positive and Negative;
+*/
+#include <string.h>
+
+int main () {
+    
+    int i, n, evens = 0, odds = 0, positives = 0, negatives = 0;
+
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &n);
+        if (n % 2 == 0) {
+            evens++;
+            if (n > 0) {
+                positives++;
+            } else if (n < 0){
+                negatives++;
+            }            
+        } else {
+            odds++;
+            if (n > 0) {
+                positives++;
+            } else if (n < 0) {
+                negatives++;
+            }           
+        }
+    }
+    printf("%d valor(es) par(es)\n", evens);
+    printf("%d valor(es) impar(es)\n", odds);  
+    printf("%d valor(es) positivo(s)\n", positives);  
+    printf("%d valor(es) negativo(s)\n", negatives);  
+    
+    return 0;
+}
