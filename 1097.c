21205a558352b301360fe4b9872a03d9b689157e
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-01-23 12:02:36 -0300

    Sequence IJ 3

---
+++ 1097.c
@@ -0,0 +1,19 @@
+/*  
+    @autor: Guiventura6;
+    @data: 23/01/2022;
+    @Problema: Sequence IJ 3;
+*/
+#include <stdio.h>
+
+int main() {
+
+    int i, j, n = 7;
+
+    for (i = 1; i <= 9; i += 2) { 
+        for (j = n; j >= n-2; j--) {
+            printf("I=%d J=%d\n", i, j);
+        }
+        n += 2;               
+    }
+    return 0;
+}
