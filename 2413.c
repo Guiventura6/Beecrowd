fac76e9d88444eb8aeb7423fb5612e5a043a988d
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-02-01 16:47:13 -0300

    Busca na internet

---
+++ 2413.c
@@ -0,0 +1,17 @@
+/*  
+    @autor: Guiventura6;
+    @data: 01/02/2022;
+    @Problema: Busca na internet;
+*/
+#include <stdio.h>
+
+int main() {
+
+    int n;
+
+    scanf("%d", &n);
+
+    printf("%d\n", n * 4);
+    
+    return 0;
+}
