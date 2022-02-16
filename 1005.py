a5a90057e4e4af9ffa42a14944edf65c5ffdacbf
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-02-15 22:28:15 -0300

    Average 1

---
+++ 1005.py
@@ -0,0 +1,11 @@
+#   @autor: Guiventura6;
+#   @data: 15/02/2022;
+#   @Problema: Average 1;
+
+a = float(input())
+b = float(input())
+
+average = (a * 3.5 + b * 7.5) / (3.5 + 7.5)
+
+print('MEDIA = {:.5f}'.format(average))
+
