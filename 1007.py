6ee67b22ed5fe5c3e43ba076f0e0392972efeb56
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-02-17 22:40:47 -0300

    Difference

---
+++ 1007.py
@@ -0,0 +1,13 @@
+#   @autor: Guiventura6;
+#   @data: 17/02/2022;
+#   @Problema: Difference;
+
+num_one = int(input())
+num_two = int(input())
+num_three = int(input())
+num_four = int(input())
+
+diference = num_one * num_two - num_three * num_four
+
+print('DIFERENCA = {}'.format(diference))
+
