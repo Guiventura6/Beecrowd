187501184eebcc2c5c4b72d820205a0acc879224
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-03-01 19:30:57 -0300

    Salary with Bonus

---
+++ 1009.py
@@ -0,0 +1,24 @@
+''' 
+    Autor: Guiventura6;
+    Data: 01/03/2022;
+    Problema: Salary with Bonus;
+    Resume: Program that reads data from a seller and calculates your salary with bonus.
+'''
+def calculate_bonus(sales):
+    '''Receives total sales and calculates 15% bonus.
+    Parameters:
+        sales (float): seller's total sales
+
+    Returns:
+        bonus (float): total bonus receivable
+    '''    
+    return sales * 0.15
+
+seller_name = input()
+salary = float(input())
+total_sales = float(input())
+
+total_salary =  salary + calculate_bonus(total_sales)
+
+print('TOTAL = R$ {:.2f}'.format(total_salary))
+
