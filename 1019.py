6733ebf20954f8e78feb24f1d235870a360a6715
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-03-03 13:23:02 -0300

    Time Conversion

---
+++ 1019.py
@@ -0,0 +1,16 @@
+''' 
+    Autor: Guiventura6;
+    Data: 03/03/2022;
+    Problema: Time Conversion;
+    Resume: Read an integer value, which is the duration in seconds of a certain event in a factory, 
+    and inform it expressed in hours:minutes:seconds.
+''' 
+duration = 0 
+
+duration = int(input())
+
+m, s = divmod(duration, 60)
+h, m = divmod(m, 60)
+
+print('{:d}:{:d}:{:d}'.format(h, m, s))
+
