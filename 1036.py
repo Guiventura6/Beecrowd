b34e6041d431f87eeb9fdea86c58b59b12c6141e
Guilherme Ventura <guilhermeventurasousa@gmail.com>
2022-03-06 16:55:24 -0300

    Bhaskara's Formula

--- 1036.py
+++ 1036.py
@@ -6,17 +6,23 @@
 '''
 import math
 
+class Error(Exception):
+    pass
+
+class ImpossibleCalculateError(Error):
+    def __init__(self, message):
+        self.message = message
+
 if __name__ == '__main__':
-
-    a, b, c = map(float, input().split())
-
-    delta = b ** 2.0 - 4 * a * c
-
-    if a == 0 or delta < 0:
-        print('Impossivel calcular')
+    try:
+        a, b, c = map(float, input().split())
+        delta = b ** 2.0 - 4 * a * c
+        if a == 0 or delta < 0:
+            raise ImpossibleCalculateError('Impossivel calcular')
+    except ImpossibleCalculateError as ex:
+        print(ex)
     else:
         r1 = (-b + math.sqrt(delta)) / (2 * a)
         r2 = (-b - math.sqrt(delta)) / (2 * a)
         print('R1 = {:.5f}'.format(r1))
         print('R2 = {:.5f}'.format(r2))
-
