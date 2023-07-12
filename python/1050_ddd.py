affeb3265f0d50ac27a79eb40474fd7145b35f28
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-07-11 22:52:34 -0300

    Include problem 1050_ddd

---
+++ python/1050_ddd.py
@@ -0,0 +1,29 @@
+'''
+    Autor: Guiventura6
+    Date: 11/07/2023
+    Modified: ()
+    Problem: ddd
+    Resume: Program that read an integer number that is the code number for
+    phone dialing
+'''
+
+cod_number = int(input())
+
+if cod_number == 61:
+    print("Brasilia")
+elif cod_number == 71:
+    print("Salvador")
+elif cod_number == 11:
+    print("Sao Paulo")
+elif cod_number == 21:
+    print("Rio de Janeiro")
+elif cod_number == 32:
+    print("Juiz de Fora")
+elif cod_number == 19:
+    print("Campinas")
+elif cod_number == 27:
+    print("Vitoria")
+elif cod_number == 31:
+    print("Belo Horizonte")
+else:
+    print("DDD nao cadastrado")
