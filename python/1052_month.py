2dee639fce5dac41baa4d178db4ab3f9fce9ac97
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-07-24 18:56:04 -0300

    Include problem 1052_month

---
+++ python/1052_month.py
@@ -0,0 +1,34 @@
+'''
+    Autor: Guiventura6
+    Date: 24/07/2023
+    Modified: ()
+    Problem: Month
+    Resume: Program that read an integer number between 1 and 12, including.
+    Corresponding to this number, print the month of the year.
+'''
+month = int(input())
+
+if month == 1:
+    print("January")
+elif month == 2:
+    print("February")
+elif month == 3:
+    print("March")
+elif month == 4:
+    print("April")
+elif month == 5:
+    print("May")
+elif month == 6:
+    print("June")
+elif month == 7:
+    print("July")
+elif month == 8:
+    print("August")
+elif month == 9:
+    print("September")
+elif month == 10:
+    print("October")
+elif month == 11:
+    print("November")
+elif month == 12:
+    print("December")
