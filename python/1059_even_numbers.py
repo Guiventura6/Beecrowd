1edc2919c6e4e452bff461ca4dd31e3078e0e910
Guiventura6 <guilhermeventurasousa@gmail.com>
2023-07-29 11:20:22 -0300

    Include problem 1059_even_numbers

---
+++ python/1059_even_numbers.py
@@ -0,0 +1,12 @@
+'''
+    Autor: Guiventura6
+    Date: 29/07/2023
+    Modified: ()
+    Problem: Even Numbers
+    Resume: Program that rints all even numbers between 1 and 100, including
+    them if it is the case.
+'''
+
+for n in range(1, 101):
+    if n % 2 == 0:
+        print(n)
